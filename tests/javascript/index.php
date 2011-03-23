67a7dd36907928683fa7a3e86a0aab4ee9756070
lds <lds2012@gmail.om>
2011-03-23 12:49:15 +0800

    新增js运行所有单元测试页面, 引入ant做自动化.

---
+++ tests/javascript/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+    <iframe src="ediary-test.html" width="500" height="500" frameborder="0"></iframe>
+    <iframe src="ediary-editor-test.html" width="500" height="500" frameborder="0"></iframe>
+</body>
+</html>
